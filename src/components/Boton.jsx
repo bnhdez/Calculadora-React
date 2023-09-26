ce7bdd35faf026cf22c3254fbaa9f4492fb3b6b4
borishdez <borisnandez93@gmail.com>
2023-09-26 00:23:00 -0600

    Creacion archivo Boton.jsx

---
+++ src/components/Boton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Boton extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <button>
+
+        </button>
+    }
+
+}
+
+export default Boton

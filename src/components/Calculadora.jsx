e67543ed0578c8ef53b76084c03930e6935b0363
borishdez <borisnandez93@gmail.com>
2023-09-26 00:05:59 -0600

    Calculadora.jsx: Creacion constructor

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -3,9 +3,14 @@ import Pantalla from "./Pantalla";
 
 class Calculadora extends React.Component{
 
+    constructor(props){
+        super(props)
+        this.state = {num1: 0}
+    }
+
     render(){
         return <div>
-            <Pantalla></Pantalla>
+            <Pantalla>{this.state.num1}</Pantalla>
         </div>
     }
 

0207c4e6e1e40cc0eba6e836494ca51b253b9f9f
borishdez <borisnandez93@gmail.com>
2023-09-25 23:56:07 -0600

    Pantalla.jsx: input y exportamos

--- src/components/Pantalla.jsx
+++ src/components/Pantalla.jsx
@@ -4,8 +4,10 @@ class Pantalla extends React.Component{
 
     render(){
         return <div>
-            
+            <input type="text" />
         </div>
     }
 
 }
+
+export default Pantalla

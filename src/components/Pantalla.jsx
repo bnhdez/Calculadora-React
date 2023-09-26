5980b8d6df4440964492404e0a932e5d29ebe6bf
borishdez <borisnandez93@gmail.com>
2023-09-26 00:17:18 -0600

    Pantalla.jsx: Creacion constructor, recibe props

--- src/components/Pantalla.jsx
+++ src/components/Pantalla.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 class Pantalla extends React.Component{
 
+    constructor(props){
+        super(props);{
+            console.log(props)
+        }
+    }
+
     render(){
         return <div>
-            <input type="text" className="form-control"/>
+            <input type="text" className="form-control" value={this.props.children}/>
         </div>
     }
 

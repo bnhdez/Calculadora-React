806338b6eb8482b525d2f89d493579298e9784ea
borishdez <borisnandez93@gmail.com>
2023-09-26 00:26:20 -0600

    agremos Boton a Calculadora

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Pantalla from "./Pantalla";
+import Boton from "./Boton";
 
 class Calculadora extends React.Component{
 
@@ -11,6 +12,9 @@ class Calculadora extends React.Component{
     render(){
         return <div>
             <Pantalla>{this.state.num1}</Pantalla>
+            <div className="row">
+                <Boton>0</Boton>
+            </div>
         </div>
     }
 

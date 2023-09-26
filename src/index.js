e6bafa85107a100c7181239065cc80440505ce1c
borishdez <borisnandez93@gmail.com>
2023-09-25 23:32:19 -0600

    Importamos Calculadora a index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Calculadora from './components/Calculadora';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+  <div>
+    <Calculadora />
+  </div>
 );

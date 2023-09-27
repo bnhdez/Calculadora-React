2cb7f3a423129fc009e4553b1749c6e47039a419
borishdez <borisnandez93@gmail.com>
2023-09-26 21:12:04 -0600

    Demas botones agregados

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -15,6 +15,22 @@ class Calculadora extends React.Component{
             <div className="row">
                 <Boton col={9}>CE</Boton>
                 <Boton>{"<"}</Boton>
+                <Boton>7</Boton>
+                <Boton>8</Boton>
+                <Boton>9</Boton>
+                <Boton>/</Boton>
+                <Boton>4</Boton>
+                <Boton>5</Boton>
+                <Boton>6</Boton>
+                <Boton>*</Boton>
+                <Boton>1</Boton>
+                <Boton>2</Boton>
+                <Boton>3</Boton>
+                <Boton>-</Boton>
+                <Boton>0</Boton>
+                <Boton>.</Boton>
+                <Boton>=</Boton>
+                <Boton>+</Boton>
             </div>
         </div>
     }
